8fb021a827f149777c9b854006be442ae92a40ef
Reece Russell <reece@prominentmedia.com>
2020-04-24 11:59:29 +0100

    added user persistence

--- core/users/user.go
+++
@@ -1,8 +0,0 @@
-package users
-
-type User struct {
-	ID           string `json:"id"`
-	Username     string `json:"username"`
-	Email        string `json:"email"`
-	PasswordHash string `json:"passwordHash"`
-}
