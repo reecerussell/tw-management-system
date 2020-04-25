8d7ce5bb1c024fa8e6095fa8a25613bd576cf513
Reece Russell <reece@prominentmedia.com>
2020-04-25 15:29:32 +0100

    added logging

--- lambda.authorizer/main.go
+++ lambda.authorizer/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/rsa"
 	"errors"
+	"log"
 	"os"
 	"strings"
 
@@ -13,7 +14,10 @@ import (
 	"github.com/reecerussell/tw-management-system/core/jwt"
 )
 
-var publicKey *rsa.PublicKey
+var (
+	publicKey *rsa.PublicKey
+	errLog    *log.Logger
+)
 
 func init() {
 	s, err := core.NewSecret(os.Getenv("SECRET_NAME"))
@@ -27,30 +31,36 @@ func init() {
 	}
 
 	publicKey = pk
+	errLog = log.New(os.Stderr, "[AUTHORIZER][ERROR]: ", log.LstdFlags)
 }
 
 // HandleAuthorize is a Handler function for lambda.
 func HandleAuthorize(evt events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
 	parts := strings.Split(evt.AuthorizationToken, " ")
 	if len(parts) < 2 {
+		errLog.Printf("invalid token format: %s", evt.AuthorizationToken)
 		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Unauthorized")
 	}
 
 	if parts[0] != "Bearer" {
+		errLog.Printf("invalid token scheme: %s", parts[0])
 		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Unauthorized")
 	}
 
 	token, err := jwt.FromToken([]byte(parts[1]))
 	if err != nil {
+		errLog.Printf("token error: %v", err)
 		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Unauthorized")
 	}
 
 	valid, err := token.Check(publicKey)
 	if err != nil {
+		errLog.Printf("invalid token: %v", err)
 		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Unauthorized")
 	}
 
 	if !valid {
+		errLog.Printf("token not valid: %v", err)
 		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Unauthorized")
 	}
 
