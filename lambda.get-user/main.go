ff7580b59e4d33758637e8b795b7ad06f6402df4
Reece Russell <reece@prominentmedia.com>
2020-04-25 19:33:57 +0100

    get-user function

---
+++ lambda.get-user/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+
+	usrs "github.com/reecerussell/tw-management-system/core/users"
+	"github.com/reecerussell/tw-management-system/core/users/usecase"
+)
+
+var users usecase.UserUsecase
+
+func init() {
+	users = usrs.Usecase()
+}
+
+func HandleGetUser(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	id, ok := req.PathParameters["id"]
+	if !ok || id == "" {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusNotFound,
+			Body:       http.StatusText(http.StatusNotFound),
+		}, nil
+	}
+
+	user, err := users.Get(id)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: err.Status(),
+			Body:       http.StatusText(http.StatusNotFound),
+		}, nil
+	}
+
+	json, _ := json.Marshal(user)
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       string(json),
+	}, nil
+}
+
+func main() {
+	lambda.Start(HandleGetUser)
+}
