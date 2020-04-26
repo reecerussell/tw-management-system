c91f7ba2b6de8448dd669e7968e5ff1e60bf7382
Reece Russell <reece@prominentmedia.com>
2020-04-26 13:13:03 +0100

    log

--- lambda.get-user/main.go
+++ lambda.get-user/main.go
@@ -19,6 +19,16 @@ func init() {
 }
 
 func HandleGetUser(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Println("Path Parameters")
+	for k, v := range req.PathParameters {
+		log.Printf("%s: %s", k, v)
+	}
+
+	log.Println("Query Parameters")
+	for k, v := range req.QueryStringParameters {
+		log.Printf("%s: %s", k, v)
+	}
+
 	id, ok := req.PathParameters["id"]
 	log.Printf("ID: %s, %v\n", id, ok)
 
