cc8562d1390e46ff8293747465c583b066dd6989
Reece Russell <reece@prominentmedia.com>
2020-04-27 18:14:51 +0100

    added create user lambda

---
+++ lambda.create-user/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/reecerussell/tw-management-system/core"
+	usrs "github.com/reecerussell/tw-management-system/core/users"
+	"github.com/reecerussell/tw-management-system/core/users/dto"
+	"github.com/reecerussell/tw-management-system/core/users/usecase"
+)
+
+var users usecase.UserUsecase
+
+func init() {
+	users = usrs.Usecase()
+}
+
+func HandleCreate(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var data dto.CreateUser
+
+	body := make([]byte, base64.StdEncoding.DecodedLen(len(req.Body)))
+	log.Printf("Body: %v\n", req.Body)
+	if req.IsBase64Encoded {
+		base64.StdEncoding.Decode(body, []byte(req.Body))
+	} else {
+		body = []byte(req.Body)
+	}
+
+	buf := bytes.NewBuffer(body)
+	_ = json.NewDecoder(buf).Decode(&data)
+
+	user, err := users.Create(&data)
+	if err != nil {
+		resp := err.Response()
+		resp.Headers = core.CORSHeaders(http.MethodPost)
+		return resp, nil
+	}
+
+	bytes, _ := json.Marshal(user)
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Headers:    core.CORSHeaders(http.MethodPost),
+		Body:       string(bytes),
+	}, nil
+}
+
+func main() {
+	lambda.Start(HandleCreate)
+}
