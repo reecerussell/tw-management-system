dae49849e16828fa720e7cc58f7457ec6e3cd617
Reece Russell <reece@prominentmedia.com>
2020-04-29 15:21:33 +0100

    added func to enable and disable queue busters

---
+++ lambda.enable-queue-buster/README.md
@@ -0,0 +1,9 @@
+# Enable Queue Buster
+
+A lambda function used to enable a queue buster via a HTTP POST request.
+
+**NOTE:** this lambda function should require authorization via the API Gateway.
+
+## Permissions
+
+This function requires full read/write access to DynamoDB.
---
+++ lambda.enable-queue-buster/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/reecerussell/tw-management-system/core"
+	"github.com/reecerussell/tw-management-system/core/queuebuster"
+	"github.com/reecerussell/tw-management-system/core/queuebuster/usecase"
+)
+
+var queueBusters usecase.QueueBusterUsecase
+
+func init() {
+	queueBusters = queuebuster.Usecase()
+}
+
+// Handle is the lambda function handler.
+func Handle(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	department := req.PathParameters["department"]
+	if department == "" {
+		resp := core.NewErrorWithStatus(
+			fmt.Errorf("missing department"),
+			http.StatusBadRequest,
+		).Response()
+		resp.Headers = core.CORSHeaders(http.MethodPost)
+		return resp, nil
+	}
+
+	err := queueBusters.Enable(department)
+	if err != nil {
+		resp := err.Response()
+		resp.Headers = core.CORSHeaders(http.MethodPost)
+		return resp, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Headers:    core.CORSHeaders(http.MethodPost),
+	}, nil
+}
+
+func main() {
+	lambda.Start(Handle)
+}
