0141a0e14c6ea31a721ed16c33ecd46bd0257e85
Reece Russell <reece@prominentmedia.com>
2020-05-11 21:17:36 +0100

    added interval to check user status

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,44 +1,52 @@
-import React, { Component } from "react";
+import React, { Suspense } from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
+import { IsAuthenticated, Logout } from "./utils/user";
 import "./App.scss";
 
-const loading = () => (
-	<div className="animated fadeIn pt-3 text-center">Loading...</div>
-);
-
-// Containers
 const Authorize = React.lazy(() => import("./containers/auth/authorize"));
 const Layout = React.lazy(() => import("./containers/layout"));
-
-// Pages
 const Login = React.lazy(() => import("./views/pages/login"));
 
-class App extends Component {
-	render() {
-		return (
-			<HashRouter>
-				<React.Suspense fallback={loading()}>
-					<Switch>
-						<Route
-							exact
-							path="/login"
-							name="Login Page"
-							render={(props) => <Login {...props} />}
-						/>
-						<Route
-							path="/"
-							name="Home"
-							render={(props) => (
-								<Authorize>
-									<Layout {...props} />
-								</Authorize>
-							)}
-						/>
-					</Switch>
-				</React.Suspense>
-			</HashRouter>
-		);
-	}
-}
+const App = () => {
+	const loadingFallback = (
+		<div className="animated fadeIn pt-3 text-center">Loading...</div>
+	);
+
+	setInterval(() => {
+		/*
+			Timeout handler. Every 15 seconds check if the user is still
+			authenticated. If the user isn't call Logout, which triggers
+			the Login page to show.
+		*/
+
+		if (!IsAuthenticated()) {
+			Logout();
+		}
+	}, 15000);
+
+	return (
+		<HashRouter>
+			<Suspense fallback={loadingFallback}>
+				<Switch>
+					<Route
+						exact
+						path="/login"
+						name="Login Page"
+						render={(props) => <Login {...props} />}
+					/>
+					<Route
+						path="/"
+						name="Home"
+						render={(props) => (
+							<Authorize>
+								<Layout {...props} />
+							</Authorize>
+						)}
+					/>
+				</Switch>
+			</Suspense>
+		</HashRouter>
+	);
+};
 
 export default App;
